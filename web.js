fdd7da868ef65ac63a84b6720b6efe72ead853a3
Cristian Monforte <cristian.monforte@gmail.com>
2013-07-21 19:17:01 +0000

    read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs =require('fs');
+var buffer = require('buffer');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+   var binaryString=fs.readFileSync('./index.html');
+    var textString=binaryString.toString();
+    response.send(textString);
 });
 
 var port = process.env.PORT || 5000;
